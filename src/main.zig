49d00eb3938d4ddf7a15cde77c26c4b891b3dea4
Jamie Atkinson <jamie.atkinson@opencorporates.com>
2024-12-27 22:50:30 +0000

    echo

--- src/main.zig
+++ src/main.zig
@@ -6,19 +6,25 @@ pub fn main() !void {
 
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
+    var command: []u8 = undefined;
+    var args: ?[]u8 = undefined;
 
     while (true) {
         try stdout.print("$ ", .{});
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
         // TODO: Handle user input
-        const space = std.mem.indexOf(u8, user_input, " ").?;
-        const command = user_input[0..space];
-        const args = user_input[space + 1 ..];
+        if (std.mem.indexOf(u8, user_input, " ")) |i| {
+            command = user_input[0..i];
+            args = user_input[i + 1 ..];
+        } else {
+            command = user_input;
+            args = null;
+        }
         if (std.mem.eql(u8, command, "exit")) {
-            std.process.exit(try std.fmt.parseInt(u8, user_input[space + 1 ..], 10));
+            std.process.exit(try std.fmt.parseInt(u8, args.?, 10));
         } else if (std.mem.eql(u8, command, "echo")) {
-            try stdout.print("{s}\n", .{args});
+            try stdout.print("{s}\n", .{args.?});
         } else {
             try stdout.print("{s}: command not found\n", .{command});
         }
