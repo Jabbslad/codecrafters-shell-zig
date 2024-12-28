9eb437d108a440e698bac5982ef499d5bda38ef6
Jamie Atkinson <jamie.atkinson@opencorporates.com>
2024-12-28 01:14:36 +0000

    executable

--- src/main.zig
+++ src/main.zig
@@ -38,7 +38,11 @@ pub fn main() !void {
                     if (is_builtin(args)) {
                         try stdout.print("{s} is a shell builtin\n", .{args});
                     } else {
-                        try check_path(allocator, stdout, args);
+                        if (try check_path(allocator, args)) |p| {
+                            try stdout.print("{s} is {s}\n", .{ args, p });
+                        } else {
+                            try stdout.print("{s}: not found\n", .{args});
+                        }
                     }
                 },
             }
@@ -52,14 +56,13 @@ fn is_builtin(command: []const u8) bool {
     return std.meta.stringToEnum(Command, command) != null;
 }
 
-fn check_path(allocator: std.mem.Allocator, stdout: anytype, command: []const u8) !void {
+fn check_path(allocator: std.mem.Allocator, command: []const u8) !?[]const u8 {
     const path = std.posix.getenv("PATH").?;
     var itr = std.mem.splitScalar(u8, path, ':');
     while (itr.next()) |pathc| {
         const bin = try std.fmt.allocPrint(allocator, "{s}{s}{s}", .{ pathc, "/", command });
         std.fs.accessAbsolute(bin, .{}) catch continue;
-        try stdout.print("{s} is {s}\n", .{ command, bin });
-        return;
+        return bin;
     }
-    try stdout.print("{s}: not found\n", .{command});
+    return null;
 }
