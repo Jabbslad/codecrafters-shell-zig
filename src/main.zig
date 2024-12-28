c0a73c1ee5b8418091ab4af41566798d598687fc
Jamie Atkinson <jamie.atkinson@opencorporates.com>
2024-12-28 00:41:11 +0000

    executable

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,10 @@ pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
 
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     while (true) {
         try stdout.print("$ ", .{});
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
@@ -34,7 +38,7 @@ pub fn main() !void {
                     if (is_builtin(args)) {
                         try stdout.print("{s} is a shell builtin\n", .{args});
                     } else {
-                        try stdout.print("{s}: not found\n", .{args});
+                        try check_path(allocator, stdout, args);
                     }
                 },
             }
@@ -46,4 +50,15 @@ pub fn main() !void {
 
 fn is_builtin(command: []const u8) bool {
     return std.meta.stringToEnum(Command, command) != null;
+}
+
+fn check_path(allocator: std.mem.Allocator, stdout: anytype, command: []const u8) !void {
+    const path = std.posix.getenv("PATH").?;
+    var itr = std.mem.splitScalar(u8, path, ':');
+    while (itr.next()) |pathc| {
+        const bin = try std.fmt.allocPrint(allocator, "{s}{s}{s}", .{ pathc, "/", command });
+        std.fs.accessAbsolute(bin, .{}) catch continue;
+        try stdout.print("{s} is {s}\n", .{ command, pathc });
+    }
+    try stdout.print("{s}: not found\n", .{command});
 }
